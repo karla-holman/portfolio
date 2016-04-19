49731547be90b2d9886949e49e49fc7ddc3b5fbf
Karla Holman <holmankarla@gmail.com>
2016-04-19 15:06:38 -0700

    Fixed contact

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   get 'home' => 'info#home'
+  post 'contact' => 'info#contact'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
