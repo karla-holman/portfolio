db51643a6e246945019ae07eb42658fc46cfd1be
Karla Holman <holmankarla@gmail.com>
2017-04-04 09:31:34 -0700

    configure projects and mail settings

--- config/application.rb
+++ config/application.rb
@@ -25,18 +25,8 @@ module Portfolio
 
     # Handle assets
     config.assets.paths << Rails.root.join("app", "assets", "styleshhets", "modules")
-    
+
     # Deliver mail
     config.action_mailer.perform_deliveries = true
-
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            'holmankarla@gmail.com',
-      password:             ENV["GMAIL_PASSWORD"],
-      :authentication =>       :plain,
-      enable_starttls_auto: true  }
   end
 end
